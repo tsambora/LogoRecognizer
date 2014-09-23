b5adbc3fb899dc3fa631c060a84dedcf05b7f427
krisnasambora <krisnasambora@yahoo.com>
2014-09-23 19:47:10 +0700

    prototype alpha build v1

---
+++ LogoDetectionFANET45/LogoDetectionFANET45/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.GPU;
+
+namespace LogoDetectionFANET45
+{
+    public partial class Form1 : Form
+    {
+        Image<Gray, Byte> model = new Image<Gray, Byte>("C:/Users/Onit/Documents/GitHub/logo-detector/test images/1.jpg");
+        Image<Gray, Byte> observed = new Image<Gray, Byte>("C:/Users/Onit/Documents/GitHub/logo-detector/test images/8.jpg");
+        int algorithm;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Image<Bgr, Byte> FAST(Image<Gray, Byte> modelImage, Image<Gray, byte> observedImage)
+        {
+            long matchTime;
+            Stopwatch watch;
+
+            HomographyMatrix homography = null;
+
+            FastDetector fastCPU = new FastDetector(10, true);
+            VectorOfKeyPoint modelKeyPoints;
+            VectorOfKeyPoint observedKeyPoints;
+            Matrix<int> indices;
+
+            BriefDescriptorExtractor descriptor = new BriefDescriptorExtractor();
+
+            Matrix<byte> mask;
+            int k = 2;
+            double uniquenessThreshold = 0.8;
+
+            watch = Stopwatch.StartNew();
+
+            //extract features from the object image
+            modelKeyPoints = fastCPU.DetectKeyPointsRaw(modelImage, null);
+            Matrix<Byte> modelDescriptors = descriptor.ComputeDescriptorsRaw(modelImage, null, modelKeyPoints);
+
+            // extract features from the observed image
+            observedKeyPoints = fastCPU.DetectKeyPointsRaw(observedImage, null);
+            Matrix<Byte> observedDescriptors = descriptor.ComputeDescriptorsRaw(observedImage, null, observedKeyPoints);
+            BruteForceMatcher<Byte> matcher = new BruteForceMatcher<Byte>(DistanceType.L2);
+            matcher.Add(modelDescriptors);
+
+            indices = new Matrix<int>(observedDescriptors.Rows, k);
+            using (Matrix<float> dist = new Matrix<float>(observedDescriptors.Rows, k))
+            {
+                matcher.KnnMatch(observedDescriptors, indices, dist, k, null);
+                mask = new Matrix<byte>(dist.Rows, 1);
+                mask.SetValue(255);
+                Features2DToolbox.VoteForUniqueness(dist, uniquenessThreshold, mask);
+            }
+
+            int nonZeroCount = CvInvoke.cvCountNonZero(mask);
+            if (nonZeroCount >= 4)
+            {
+                nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints, indices, mask, 1.5, 20);
+                if (nonZeroCount >= 4)
+                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(
+                    modelKeyPoints, observedKeyPoints, indices, mask, 2);
+            }
+
+            watch.Stop();
+
+            //Draw the matched keypoints
+            Image<Bgr, Byte> result = Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
+               indices, new Bgr(255, 255, 255), new Bgr(255, 255, 255), mask, Features2DToolbox.KeypointDrawType.DEFAULT);
+
+            #region draw the projected region on the image
+            if (homography != null)
+            {  //draw a rectangle along the projected model
+                Rectangle rect = modelImage.ROI;
+                PointF[] pts = new PointF[] { 
+                 new PointF(rect.Left, rect.Bottom),
+                 new PointF(rect.Right, rect.Bottom),
+                 new PointF(rect.Right, rect.Top),
+                 new PointF(rect.Left, rect.Top)};
+                homography.ProjectPoints(pts);
+
+                result.DrawPolyline(Array.ConvertAll<PointF, Point>(pts, Point.Round), true, new Bgr(Color.Red), 5);
+            }
+            #endregion
+
+            matchTime = watch.ElapsedMilliseconds;
+            richTextBox1.Clear();
+            richTextBox1.AppendText("FAST time = " + matchTime);
+
+            return result;
+        }
+
+        public Image<Bgr, Byte> SURF(Image<Gray, Byte> modelImage, Image<Gray, byte> observedImage)
+        {
+            long matchTime;
+            Stopwatch watch;
+            HomographyMatrix homography = null;
+
+            SURFDetector surfCPU = new SURFDetector(500, false);
+            VectorOfKeyPoint modelKeyPoints;
+            VectorOfKeyPoint observedKeyPoints;
+            Matrix<int> indices;
+
+            Matrix<byte> mask;
+            int k = 2;
+            double uniquenessThreshold = 0.8;
+
+            watch = Stopwatch.StartNew();
+
+            //extract features from the object image
+            modelKeyPoints = surfCPU.DetectKeyPointsRaw(modelImage, null);
+            Matrix<float> modelDescriptors = surfCPU.ComputeDescriptorsRaw(modelImage, null, modelKeyPoints);
+
+            // extract features from the observed image
+            observedKeyPoints = surfCPU.DetectKeyPointsRaw(observedImage, null);
+            Matrix<float> observedDescriptors = surfCPU.ComputeDescriptorsRaw(observedImage, null, observedKeyPoints);
+            BruteForceMatcher<float> matcher = new BruteForceMatcher<float>(DistanceType.L2);
+            matcher.Add(modelDescriptors);
+
+            indices = new Matrix<int>(observedDescriptors.Rows, k);
+            using (Matrix<float> dist = new Matrix<float>(observedDescriptors.Rows, k))
+            {
+                matcher.KnnMatch(observedDescriptors, indices, dist, k, null);
+                mask = new Matrix<byte>(dist.Rows, 1);
+                mask.SetValue(255);
+                Features2DToolbox.VoteForUniqueness(dist, uniquenessThreshold, mask);
+            }
+
+            int nonZeroCount = CvInvoke.cvCountNonZero(mask);
+            if (nonZeroCount >= 4)
+            {
+                nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints, indices, mask, 1.5, 20);
+                if (nonZeroCount >= 4)
+                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints, observedKeyPoints, indices, mask, 2);
+            }
+
+            watch.Stop();
+
+            //Draw the matched keypoints
+            Image<Bgr, Byte> result = Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
+               indices, new Bgr(255, 255, 255), new Bgr(255, 255, 255), mask, Features2DToolbox.KeypointDrawType.DEFAULT);
+
+            #region draw the projected region on the image
+            if (homography != null)
+            {  //draw a rectangle along the projected model
+                Rectangle rect = modelImage.ROI;
+                PointF[] pts = new PointF[] { 
+               new PointF(rect.Left, rect.Bottom),
+               new PointF(rect.Right, rect.Bottom),
+               new PointF(rect.Right, rect.Top),
+               new PointF(rect.Left, rect.Top)};
+                homography.ProjectPoints(pts);
+
+                result.DrawPolyline(Array.ConvertAll<PointF, Point>(pts, Point.Round), true, new Bgr(Color.Red), 5);
+            }
+            #endregion
+
+            matchTime = watch.ElapsedMilliseconds;
+            richTextBox1.Clear();
+            richTextBox1.AppendText("SURF time = " + matchTime);
+
+            return result;
+        }
+
+        public Image<Bgr, Byte> SIFT(Image<Gray, Byte> modelImage, Image<Gray, byte> observedImage)
+        {
+            long matchTime;
+            Stopwatch watch;
+            HomographyMatrix homography = null;
+
+            SIFTDetector siftCPU = new SIFTDetector();
+            VectorOfKeyPoint modelKeyPoints;
+            VectorOfKeyPoint observedKeyPoints;
+            Matrix<int> indices;
+
+            Matrix<byte> mask;
+            int k = 2;
+            double uniquenessThreshold = 0.8;
+
+            watch = Stopwatch.StartNew();
+
+            //extract features from the object image
+            modelKeyPoints = siftCPU.DetectKeyPointsRaw(modelImage, null);
+            Matrix<float> modelDescriptors = siftCPU.ComputeDescriptorsRaw(modelImage, null, modelKeyPoints);
+
+            // extract features from the observed image
+            observedKeyPoints = siftCPU.DetectKeyPointsRaw(observedImage, null);
+            Matrix<float> observedDescriptors = siftCPU.ComputeDescriptorsRaw(observedImage, null, observedKeyPoints);
+            BruteForceMatcher<float> matcher = new BruteForceMatcher<float>(DistanceType.L2);
+            matcher.Add(modelDescriptors);
+
+            indices = new Matrix<int>(observedDescriptors.Rows, k);
+            using (Matrix<float> dist = new Matrix<float>(observedDescriptors.Rows, k))
+            {
+                matcher.KnnMatch(observedDescriptors, indices, dist, k, null);
+                mask = new Matrix<byte>(dist.Rows, 1);
+                mask.SetValue(255);
+                Features2DToolbox.VoteForUniqueness(dist, uniquenessThreshold, mask);
+            }
+
+            int nonZeroCount = CvInvoke.cvCountNonZero(mask);
+            if (nonZeroCount >= 4)
+            {
+                nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints, indices, mask, 1.5, 20);
+                if (nonZeroCount >= 4)
+                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints, observedKeyPoints, indices, mask, 2);
+            }
+
+            watch.Stop();
+
+            //Draw the matched keypoints
+            Image<Bgr, Byte> result = Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
+               indices, new Bgr(255, 255, 255), new Bgr(255, 255, 255), mask, Features2DToolbox.KeypointDrawType.DEFAULT);
+
+            #region draw the projected region on the image
+            if (homography != null)
+            {  //draw a rectangle along the projected model
+                Rectangle rect = modelImage.ROI;
+                PointF[] pts = new PointF[] { 
+                   new PointF(rect.Left, rect.Bottom),
+                   new PointF(rect.Right, rect.Bottom),
+                   new PointF(rect.Right, rect.Top),
+                   new PointF(rect.Left, rect.Top)};
+                homography.ProjectPoints(pts);
+
+                result.DrawPolyline(Array.ConvertAll<PointF, Point>(pts, Point.Round), true, new Bgr(Color.Red), 5);
+            }
+            #endregion
+
+            matchTime = watch.ElapsedMilliseconds;
+            richTextBox1.Clear();
+            richTextBox1.AppendText("SIFT time = " + matchTime);
+
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control control in this.groupBox1.Controls)
+            {
+                if (control is RadioButton)
+                {
+                    RadioButton radio = control as RadioButton;
+                    if (radio.Checked)
+                    {
+                        richTextBox1.Clear();
+                        richTextBox1.AppendText(radio.Text);
+                        algorithm = 1;
+                    }
+                }
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control control in this.groupBox1.Controls)
+            {
+                if (control is RadioButton)
+                {
+                    RadioButton radio = control as RadioButton;
+                    if (radio.Checked)
+                    {
+                        richTextBox1.Clear();
+                        richTextBox1.AppendText(radio.Text);
+                        algorithm = 2;
+                    }
+                }
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Control control in this.groupBox1.Controls)
+            {
+                if (control is RadioButton)
+                {
+                    RadioButton radio = control as RadioButton;
+                    if (radio.Checked)
+                    {
+                        richTextBox1.Clear();
+                        richTextBox1.AppendText(radio.Text);
+                        algorithm = 3;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "pilih gambar model";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "jpg";
+
+            openFileDialog1.RestoreDirectory = false;
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                model = new Image<Gray, Byte>(openFileDialog1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "pilih gambar target";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "jpg";
+
+            openFileDialog1.RestoreDirectory = false;
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog1.FileName;
+                observed = new Image<Gray, Byte>(openFileDialog1.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            switch (algorithm) { 
+                case 1:
+                    imageBox1.Image = FAST(model, observed);
+                    break;
+                case 2:
+                    imageBox1.Image = SURF(model, observed);
+                    break;
+                case 3:
+                    imageBox1.Image = SIFT(model, observed);
+                    break;
+            }
+        }
+    }
+}
